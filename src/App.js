ac676ef698a4214b11a46021bd7de0010cac0086
Natalie Ly <n6ly@uwaterloo.ca>
2023-11-25 22:13:38 -0500

    adjust server posting and listening

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
     }
 
     try{
-      const response = await fetch('http://localhost:8000/completions', options);
+      const response = await fetch('https://ingredientsalchemy.netlify.app/completions', options);
       const data = await response.json();
       setMessage(data.choices[0].message);
     } catch(error){
